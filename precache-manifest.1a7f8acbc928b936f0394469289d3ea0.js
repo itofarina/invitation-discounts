da86d18acef5d4cb30cfdfb2b3dabce3b38c09f7
Alvaro Farina <farinalvaro@gmail.com>
2020-01-14 19:01:41 -0300

    Updates

---
+++ precache-manifest.1a7f8acbc928b936f0394469289d3ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7f6f980fb1a63e205fb9f1a532c2e8",
+    "url": "/invitation-discounts/index.html"
+  },
+  {
+    "revision": "7cd4c521068e84c6f630",
+    "url": "/invitation-discounts/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d50920835754f64666dd",
+    "url": "/invitation-discounts/static/js/2.1e9bee6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/invitation-discounts/static/js/2.1e9bee6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cd4c521068e84c6f630",
+    "url": "/invitation-discounts/static/js/main.12cf156e.chunk.js"
+  },
+  {
+    "revision": "2f93b5794123cc8edd70",
+    "url": "/invitation-discounts/static/js/runtime-main.117b18b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/invitation-discounts/static/media/logo.5d5d9eef.svg"
+  }
+]);
