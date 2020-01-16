3db609b965afe31eb8fe926b97ad3e9528ba419a
Alvaro Farina <farinalvaro@gmail.com>
2020-01-16 19:19:54 -0300

    Added styles and content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
+
+import Footer from './components/footer/footer.component';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        Descontá 44 USD en tu reserva de Airbnb usando una invitación para unirte!
-        </p>
-        <a
-          className="App-link"
-          href="https://es.airbnb.com/c/alvarof2345?currency=USD"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Invitación
-        </a>
-      </header>
+    <div>
+      <Header />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+
+  <footer class="text-muted">
+    <div class="container">
+      <p>Este sitio es seguro. Sin publicidad y sin spam. Accedé al cupón de descuento de Airbnb a través de una invitación!</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
